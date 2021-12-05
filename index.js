25de0586e78924180eb125ba815cda98fcf07545
AmelioMansour <ameliomansour@yahoo.com>
2021-12-04 19:40:36 -0500

    added trivia

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
+const { request } = require("express");
 
 const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
-
-app.get("/", (req, res) => {    
+//Nav routes
+app.get("/", (req, res) => {
     res.render("index");
 });
 app.get("/views/index.ejs", (req, res) => {
@@ -25,6 +27,37 @@ app.get("/views/html/my_fun_facts.ejs", (req, res) => {
 });
 app.get("/views/html/blog.ejs", (req, res) => {
     res.render("./html/blog");
+});
+app.get("/views/html/trivia.ejs", (req, res) => {
+    x=null;
+    res.render("./html/trivia");
+});
+//
+app.locals.x=null;
+app.post("/getanswers/", (req, res) => {
+    let x = 0;
+    let data = {
+        a1: req.body.Answer1, a2: req.body.Answer2,
+        a3: req.body.Answer3, a4: req.body.Answer4
+    }
+    if (data.a1 == "B") {
+        x = x + 1;
+    }
+    if (data.a2 == "C") {
+        x = x + 1;
+    }
+    if (data.a3 == "A") {
+        x = x + 1;
+    }
+    if (data.a4 == "D") {
+        x = x + 1;
+    }
+    res.render('html/trivia.ejs',{x:x});
+    
+    
+    res.send(console.log(x));
+    
+
 });
 
 const PORT = process.env.PORT || 3000;
